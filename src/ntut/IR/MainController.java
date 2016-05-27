e4cc40bac0eb5c9cb54d9c954195a71aa6ef8057
Vodalok <visualcshape@gmail.com>
2016-05-28 03:51:45 +0800

    [M] Add determination statement when user did not choice a directory to MainController. [M] A little change on GUI layout. [M] Add lib to ignore directory to .gitignore

--- src/ntut/IR/MainController.java
+++ src/ntut/IR/MainController.java
@@ -2,6 +2,7 @@ package ntut.IR;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
@@ -25,7 +26,20 @@ public class MainController {
         directoryChooser.setTitle(DIRECTORY_CHOOSER_TITLE);
         directoryChooser.setInitialDirectory(new File(PWD));
         File location = directoryChooser.showDialog(mStage);
-        this.mModel.setDataSetLocation(location.getAbsolutePath());
+        if(location != null)
+            this.mModel.setDataSetLocation(location.getAbsolutePath());
+    }
+
+    @FXML
+    private void ClickAboutMenuItem(){
+        String HEADER_TEXT = "Information Retrieval : Classification";
+        String TITLE = "About";
+        String CONTENT = "Authors:\n 104598013 孫季加\n 104598050 黃仲毅\n";
+        Alert aboutAlert = new Alert(Alert.AlertType.INFORMATION);
+        aboutAlert.setHeaderText(HEADER_TEXT);
+        aboutAlert.setTitle(TITLE);
+        aboutAlert.setContentText(CONTENT);
+        aboutAlert.show();
     }
 
     public void setThisStage(Stage stage){
